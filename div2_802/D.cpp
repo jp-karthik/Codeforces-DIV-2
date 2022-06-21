3a5f8f28fa2c5d2256dfacad2fb8aa5215537645
jp-karthik <karthikjagadeeshponarkar@gmail.com>
2022-06-21 17:28:15 +0530

    802

---
+++ div2_802/D.cpp
@@ -0,0 +1,108 @@
+//TC -> O(N*log(A[i]) + Q)
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pi;
+typedef pair<long, long> pll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<pair<int,int>> vpi;
+typedef vector<pair<long long, long long>> vpll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define rep(a, b) for(ll i=a; i<=b; i++)
+#define Sort(v) sort(v.begin(), v.end())
+#define revSort(v) sort(v.begin(), v.end(), greater<ll>())
+#define all(x) (x).begin(), (x).end()
+#define input(x) for(auto& y : x) cin >> y
+#define output(x) for (auto y : x) cout << y << endl 
+#define endl "\n"
+#define PI 3.141592653589793238462
+#define MOD 1000000007
+#define INF 1e18
+
+
+#ifndef ONLINE_JUDGE
+	#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+	#define debug(x)
+#endif 
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve(int arg) {
+    ll n;
+    cin >> n;
+    vll v(n);
+    input(v);
+    //binary search to find minimum valid time O(N*log(Ai))
+    ll low = 1;
+    ll high = 1e9;
+    ll time;
+    while (low <= high) {
+    	ll mid = (low + high) / 2;
+    	ll amount = 0;
+    	bool isP = true;
+    	for (ll i = 0; i < n; i++) {
+    		amount += mid;
+    		if (amount >= v[i]) {
+    			amount -= v[i];
+    		} else {
+    			isP = false;
+    			break;
+    		}
+    	}
+    	if (isP) {
+    		time = mid;
+    		high = mid - 1;
+    	} else {
+    		low = mid + 1;
+    	}
+    }
+    ll cSum = accumulate(v.begin(), v.end(), (ll)0);
+    ll q;
+    cin >> q;
+    for (ll i = 0; i < q; i++) {
+    	ll t;
+    	cin >> t;
+    	if (t < time) {
+    		cout << -1 << endl;
+    		continue;
+    	}
+    	cout << (cSum + t - 1) / t << endl;
+    }
+}
+
+int main() {
+	#ifndef ONLINE_JUDGE
+		freopen("error.txt", "w", stderr);
+	#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	solve(1);
+	return 0;
+}
